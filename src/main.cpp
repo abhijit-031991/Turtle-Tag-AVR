66553a677395522b4a08f1d7150dd1b4933ba04d
Arcturus Inc <63253164+abhijit-031991@users.noreply.github.com>
2023-02-02 19:59:17 +0530

    V0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+#include <permaDefs.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <time.h>
+#include <TimeLib.h>
+#include <LoRa.h>
+#include <EEPROM.h>
+#include <TinyGPS++.h>
+#include <SPIMemory.h>
+#include <elapsedMillis.h>
+#include <avr/sleep.h>
+#include <MCP79412RTC.h>
+#include <DFRobot_LIS2DW12.h>
 
 void setup() {
   // put your setup code here, to run once:
---
+++ src/permaDefs.h
@@ -0,0 +1,78 @@
+////////////////////////////////////////////
+/////      Firmware Definitions        /////
+////////////////////////////////////////////
+
+///// Device Definitions /////
+
+// Device ID //
+
+const uint16_t tag = 10109;
+const uint8_t devType = 107;
+
+// Firmware Version //
+const float firmwareVersion = 2.0;
+
+//// Pin Definitions ////
+
+#define GPS_PIN PIN_PC3
+#define RTC_PIN PIN_PD7
+#define RINT PIN_PD6
+#define AINT1 PIN_PD4
+#define AINT2 PIN_PD5
+#define LCS PIN_PA7
+#define FCS PIN_PC2
+#define LRST PIN_PA3
+#define LDIO0 PIN_PF5
+#define LDIO1 PIN_PF4
+
+
+//// Structs ////
+
+//// Request Ping ////
+
+struct reqPing{
+    uint16_t tag;
+    byte request;
+  };
+
+struct resPing{
+    uint16_t tag;
+    byte resp;
+  };
+
+struct data{
+    uint32_t datetime;
+    uint16_t locktime;
+    float lat;
+    float lng;
+    byte hdop;
+    bool act;
+  };
+
+struct ping{
+    uint16_t ta;    
+    uint16_t cnt;
+    float la;
+    float ln;
+    uint8_t devtyp;
+    bool mortality;
+  }; 
+
+struct setttings{
+        uint32_t pingTime;
+        uint16_t act_trsh;
+        uint16_t act_gps_frq;
+        uint16_t act_duration;
+        uint16_t gpsFrq;
+        uint16_t gpsTout;
+        uint8_t hdop;
+        uint8_t radioFrq;
+        uint8_t rcv_dur;
+        uint8_t sch_dur;
+        uint8_t sch_rpt_dur;
+        bool act_enabled;
+        bool sch_enabled;
+      } __attribute__((__packed__));
+
+
+
